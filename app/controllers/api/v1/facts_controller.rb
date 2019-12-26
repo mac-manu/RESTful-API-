94c3b7faee4226f4df515132f592ed76c7380018
macmanu <manu@mac-manu.com>
2019-12-26 14:01:44 +0100

    users controller crud

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::FactsController < ApplicationController
+  before_action :find_fact, only: [:show, :update, :destroy]
+
+  def index
+    @facts = Fact.all
+    render json: @facts
+  end
 end
